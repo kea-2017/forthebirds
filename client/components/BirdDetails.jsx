cd49548c36a797bd85eaf0ddac8542f5c99996e1
sherlyc <sherlyc@gmail.com>
2017-08-11 12:34:58 +1200

    delete is working on client side

--- client/components/BirdDetails.jsx
+++ client/components/BirdDetails.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import {getBirds} from '../actions/birds'
+import {deleteBirdAction} from '../actions/deleteBird'
 
-const renderBird = (bird) => (
+const renderBird = (bird, dispatch) => (
   <div>
     <p>{bird.name}</p>
     <p>{bird.description}</p>
     <p>{bird.country}</p>
-     <img src={bird.imageUrl} width="500px" height="300px" />
+    <img src={bird.imageUrl} width="500px" height="300px"/>
+    <button onClick={() => dispatch(deleteBirdAction(bird))}>Lose bird</button>
+
   </div>
 )
 
@@ -17,13 +20,16 @@ const BirdDetails = ({birds, dispatch, id}) => {
   return (
     <div>
       <Link to='/'>Home</Link>
-      {bird && renderBird(bird)}
+      {bird
+        ? renderBird(bird, dispatch)
+        : document.location = '/'
+      }
     </div>
   )
 }
 const mapStateToProps = (state) => {
   console.log(state.birds);
-return {birds: state.birds}
+  return {birds: state.birds}
 }
 
 export default connect(mapStateToProps)(BirdDetails)
--- client/components/BirdList.jsx
+++ client/components/BirdList.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { Link } from 'react-router-dom'
 import {getBirds} from '../actions/birds'
+import {deleteBird} from '../actions/deleteBird'
+
 
 const renderBirds = (bird, key) => (
   <Link to={'/birds/' + bird.id}  key={key}><h3>{bird.name}</h3></Link>
@@ -15,7 +17,6 @@ const Birds = ({birds, dispatch}) => (
 
 const mapStateToProps = (state) => {
 return {birds: state.birds}
-
 }
 
 export default connect(mapStateToProps)(Birds)
